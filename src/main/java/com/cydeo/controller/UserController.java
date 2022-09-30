f9af45df216a6138faba6e9e0105212db7f8f1cf
Ekaterina <ekaterina.sementeva@gmail.com>
2022-09-30 13:21:12 -0400

    Application Structure from scratch

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,19 +1,36 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
+import com.cydeo.service.impl.RoleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-@GetMapping("/create")
-public String createUser(Model model){
+
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/create")
+    public String createUser(Model model){
     model.addAttribute("user", new UserDTO());
-model.addAttribute("roles",)
+    model.addAttribute("roles", roleService.findAll());
+    model.addAttribute("users", userService.findAll());
     return "user/create";
 }
+
+
 
 }
--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,4 +1,30 @@
 package com.cydeo.service.impl;
 
-public class UserServiceImpl {
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service// same as @Component
+public class UserServiceImpl extends AbstractMapService<UserDTO,String> implements UserService {
+    @Override
+    public UserDTO save(UserDTO user) {
+        return super.save(user.getUserName(), user);
+    }
+
+    @Override
+    public UserDTO findById(String id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(String id) {
+    super.deleteById(id);
+    }
 }
