f64372736babc0deb93f691701b7cb6d186ab379
Ekaterina <ekaterina.sementeva@gmail.com>
2022-10-02 12:14:17 -0400

    Update and Delete business Logic

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -7,10 +7,7 @@ import com.cydeo.service.UserService;
 import com.cydeo.service.impl.RoleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -29,7 +26,7 @@ public class UserController {
     model.addAttribute("user", new UserDTO());
     model.addAttribute("roles", roleService.findAll());
     model.addAttribute("users", userService.findAll());
-    return "user/create";
+    return "/user/create";
         // if you return same view, you have to provide in a method all the attributes required for the html file to show it
         // whatever view needs(object, lists etc) need to be provided again in a new method
 }
@@ -41,5 +38,14 @@ public class UserController {
         return "redirect:/user/create";//end point when you redirect
     }
 
+    @GetMapping("/update/{username}")// to bring something from html to our code we need PathVariable or Query
+    public String editUser(@PathVariable("username") String username, Model model){
+        model.addAttribute("user", userService.findById(username));
+        // need to go to DB take a required object and get the user
+        // if you do any action that required connection with DB you need to go to service
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+        return "/user/update";
+    }
 
 }
